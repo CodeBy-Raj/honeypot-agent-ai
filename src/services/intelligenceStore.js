2d8c357aa2551eea4fe3885cf97d341b7663a2e7
Harsh Raj <samarshelley@gmail.com>
2026-02-16 13:20:51 +0530

    some changes

--- src/services/intelligenceStore.js
+++ src/services/intelligenceStore.js
@@ -7,6 +7,7 @@ export function initSession(sessionId) {
     intelligenceDB.set(sessionId, {
       phishingLinks: new Set(),
       upiIds: new Set(),
+      emailAddresses: new Set(),
       phoneNumbers: new Set(),
       suspiciousKeywords: new Set(),
       bankAccounts: new Set(),
@@ -21,6 +22,7 @@ export function addIntelligence(sessionId, extracted) {
 
   extracted.phishingLinks?.forEach((l) => store.phishingLinks.add(l));
   extracted.upiIds?.forEach((u) => store.upiIds.add(u));
+  extracted.emailAddresses?.forEach((e) => store.emailAddresses.add(e));
   extracted.phoneNumbers?.forEach((p) => store.phoneNumbers.add(p));
   extracted.suspiciousKeywords?.forEach((k) => store.suspiciousKeywords.add(k));
   extracted.bankAccounts?.forEach((b) => store.bankAccounts.add(b));
@@ -33,6 +35,7 @@ export function getIntelligence(sessionId) {
   return {
     phishingLinks: [...store.phishingLinks],
     upiIds: [...store.upiIds],
+    emailAddresses: [...store.emailAddresses],
     phoneNumbers: [...store.phoneNumbers],
     suspiciousKeywords: [...store.suspiciousKeywords],
     bankAccounts: [...store.bankAccounts],
